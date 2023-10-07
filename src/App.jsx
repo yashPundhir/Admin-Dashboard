24729fe06f09f08d0bb2e9fd52f5e02d26cbb7b0
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-07 23:36:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="w-full">
+			<h1 className="text-red-500 text-center text-5xl font-bold">
+				Admin Dashboard
+			</h1>
+		</div>
+	);
+}
+
+export default App;
